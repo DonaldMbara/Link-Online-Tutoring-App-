210f6fe8788176645613409f4645ac9e9691971e
BL4CKDUDE <61325361+BL4CKDUDE@users.noreply.github.com>
2020-02-29 21:57:32 +0200

    v 1.0 First Two Screens
    Version 1.0 off Welcome Screen and login Screen.

---
+++ app/src/main/java/com/example/link_online_tutoring_app_/SplashScreenActivity.java
@@ -0,0 +1,34 @@
+package com.example.link_online_tutoring_app_;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends Activity {
+    @Override
+    protected void onCreate( Bundle bundle) {
+        super.onCreate(bundle);
+
+        setContentView(R.layout.splashscreenlayout);
+
+        final TextView tv = findViewById(R.id.WelcomeTextView);
+        final ImageView ivLogo = findViewById(R.id.LinkHands);
+        final Button CB = findViewById(R.id.ContinueButton);
+        // final ImageView ivArrow = findViewById(R.id.ContinueArrow);
+
+
+        CB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+
+            }
+        });
+    }
+
+}
