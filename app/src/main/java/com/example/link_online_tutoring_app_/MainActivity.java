8c0f3f7602ff4fbc90622606422c5e2685b50d60
Donald <50341367+DonaldMbara@users.noreply.github.com>
2020-03-09 21:00:26 +0200

    Revert "Revert "tested requestHandler""
    This reverts commit e34df4ac32dc38036bfcf372e055e10f12f1c282.

--- app/src/main/java/com/example/link_online_tutoring_app_/MainActivity.java
+++ app/src/main/java/com/example/link_online_tutoring_app_/MainActivity.java
@@ -23,42 +23,44 @@ import managers.AsyncHTTP;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText std_number,password;
-    Button login;
-    TextView Register;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        std_number = findViewById(R.id.StudentNoEditText);
-        password  = findViewById(R.id.PassEditText);
-        login = findViewById(R.id.loginBtn);
-        Register = findViewById(R.id.RegisterButton);
+        final EditText std_number= findViewById(R.id.StudentNoEditText);
+        final EditText password= findViewById(R.id.PassEditText);
+        final Button login = findViewById(R.id.button);
+        final Button Register = findViewById(R.id.RegisterButton);
 
-    }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String stdNumber = std_number.getText().toString().trim();
+                final String PassWord = password.getText().toString().trim();
+                boolean Alltrue = true;
+                if(TextUtils.isEmpty(stdNumber)){
+                    Alltrue = false;
+                    std_number.setError("Enter Student Number");
+                }
+                if(TextUtils.isEmpty(stdNumber)){
+                    Alltrue = false;
+                    password.setError("Enter Password");
+                }
 
-    public void OnClickRegisterHere(View view) {
-        startActivity(new Intent(MainActivity.this, RegisterActivity.class));
-    }
+                if(Alltrue){
+                    //in case all credentials are filled attempt login
+                   new RequestHandler(getApplicationContext(),"login").execute(stdNumber,PassWord);
 
-    public void onClickLogin(View view) {
-        final String stdNumber = std_number.getText().toString().trim();
-        final String PassWord = password.getText().toString().trim();
-        boolean Alltrue = true;
-        if (TextUtils.isEmpty(stdNumber)) {
-            Alltrue = false;
-            std_number.setError("Enter Student Number");
-        }
-        if (TextUtils.isEmpty(stdNumber)) {
-            Alltrue = false;
-            password.setError("Enter Password");
-        }
+                }
+            }
+        });
 
-        if (Alltrue) {
-            //in case all credentials are filled attempt login
-            new RequestHandler(getApplicationContext(), "login").execute(stdNumber, PassWord);
 
-        }
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
