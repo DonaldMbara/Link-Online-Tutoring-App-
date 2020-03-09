e34df4ac32dc38036bfcf372e055e10f12f1c282
Donald <50341367+DonaldMbara@users.noreply.github.com>
2020-03-09 20:46:32 +0200

    Revert "tested requestHandler"
    This reverts commit 499118be4c2cd600f1b4f966d39b0a8653f7348a.
    
    # Conflicts:
    #	.gradle/5.4.1/executionHistory/executionHistory.lock
    #	.gradle/5.4.1/fileHashes/fileHashes.bin
    #	.gradle/5.4.1/fileHashes/fileHashes.lock
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	app/src/androidTest/java/com/example/link_online_tutoring_app_/RequestHandlerTest.java

--- app/src/main/java/com/example/link_online_tutoring_app_/RegisterActivity.java
+++ app/src/main/java/com/example/link_online_tutoring_app_/RegisterActivity.java
@@ -4,7 +4,9 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,103 +19,73 @@ import managers.AsyncHTTP;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText userName ,firstName,lastName, email, studentNumber,password;
+    EditText userName, firstName, lastName, email, studentNumber, password;
     Button confirmReg;
 
+    String UserName, FirstName, LastName, Email, StudentNumber, Password;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
 
-        //final EditText idNum = findViewById(R.id.idNumTxt);
+
         userName = findViewById(R.id.userNameTxt);
-         firstName = findViewById(R.id.fNameTxt);
-         lastName = findViewById(R.id.lNameTxt);
-         email = findViewById(R.id.emailTxt);
+        firstName = findViewById(R.id.fNameTxt);
+        lastName = findViewById(R.id.lNameTxt);
+        email = findViewById(R.id.emailTxt);
         studentNumber = findViewById(R.id.stdNumTxt);
-         password = findViewById(R.id.passTxt);
-         confirmReg = findViewById(R.id.button2);
+        password = findViewById(R.id.passTxt);
+        confirmReg = findViewById(R.id.button2);
+
+        userName.addTextChangedListener(registerTextWatcher);
+        firstName.addTextChangedListener(registerTextWatcher);
+        lastName.addTextChangedListener(registerTextWatcher);
+        email.addTextChangedListener(registerTextWatcher);
+        studentNumber.addTextChangedListener(registerTextWatcher);
+        password.addTextChangedListener(registerTextWatcher);
 
 
     }
 
+    private TextWatcher registerTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+
+            UserName = userName.getText().toString().trim();
+            FirstName = firstName.getText().toString().trim();
+            LastName = lastName.getText().toString().trim();
+            Email = email.getText().toString().trim();
+            StudentNumber = studentNumber.getText().toString().trim();
+            Password = password.getText().toString().trim();
+            confirmReg.setEnabled(!FirstName.isEmpty() && !LastName.isEmpty() &&
+                    !StudentNumber.isEmpty() && !Password.isEmpty() && !Email.isEmpty()
+            );
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
     public void onClickRegister(View view) {
+        if (confirmReg.isEnabled()) {
 
-        final String UserName = userName.getText().toString().trim();
-        final String FirstName = firstName.getText().toString().trim();
-        final String LastName = lastName.getText().toString().trim();
-        final String Email = email.getText().toString().trim();
-        final String StudentNumber = studentNumber.getText().toString().trim();
-        final String Password = password.getText().toString().trim();
-        //int stdNum = Integer.parseInt(StudentNumber);
-        boolean checkMyThings = true;
-
-        final ContentValues cv = new ContentValues();
-
-        if(TextUtils.isEmpty(UserName)){
-            userName.setError("Enter User Name");
-            checkMyThings = false;
-        }
-        if(TextUtils.isEmpty(FirstName)){
-            firstName.setError("Enter First Name");
-            checkMyThings = false;
-        }
-        if(TextUtils.isEmpty(LastName)){
-            lastName.setError("Enter Last Name");
-            checkMyThings = false;
-        }
-        if(TextUtils.isEmpty(Email)){
-            email.setError("Enter Email Address");
-            checkMyThings = false;
-        }
-        if(TextUtils.isEmpty(StudentNumber)){
-            studentNumber.setError("Enter Student Number");
-            checkMyThings = false;
-        }
-        if(TextUtils.isEmpty(Password)){
-            password.setError("Enter Password");
-            checkMyThings = false;
-        }
-        Log.d("BeforeCheckTingz","BeforeCheckTings");
-
-        if(checkMyThings == true){
-
-            new RequestHandler(getApplicationContext(),"register").execute(UserName,Password,Email,FirstName,LastName,StudentNumber);
+            new RequestHandler(getApplicationContext(), "register").execute(UserName, Password, Email, FirstName, LastName, StudentNumber);
             startActivity(new Intent(RegisterActivity.this, MainActivity.class));
-        }
-        else{
-            Log.d("InElseForT","InElseForT");
-            Toast toast = Toast.makeText(getApplicationContext(),"Please fill in all required fields",Toast.LENGTH_SHORT);
+        } else {
+            Log.d("InElseForT", "InElseForT");
+            Toast toast = Toast.makeText(getApplicationContext(), "Please fill in all required fields", Toast.LENGTH_SHORT);
             toast.show();
         }
 
-
     }
 
-
-//    private static void register(ContentValues cv, final Context c){
-//        new AsyncHTTP("http://lamp.ms.wits.ac.za/~s1819369/registration.php",cv){
-//
-//
-//            @Override
-//            protected void onPreExecute() {
-//
-//            }
-//
-//            @Override
-//            protected void onPostExecute(String output) {
-//                Log.d("afterLink","afterLink");
-//                if(output!=null && output.equals("Registered Successfully")){
-//                    Toast t = Toast.makeText(c,"You can now Log In",Toast.LENGTH_SHORT);
-//                    t.show();
-//                    c.startActivity(new Intent(c,HomeActivity.class));
-//                }
-//
-//                else{
-//                    Toast t = Toast.makeText(c,"Something Went Wrong",Toast.LENGTH_SHORT);
-//                    t.show();
-//                }
-//            }
-//        }.execute();
-//    }
 }
