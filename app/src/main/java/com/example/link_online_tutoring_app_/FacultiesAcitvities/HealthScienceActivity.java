ab4e73e07767667606392776c602496b664c72cb
Donald <50341367+DonaldMbara@users.noreply.github.com>
2020-03-15 13:12:33 +0200

    I made activities for every faculties

---
+++ app/src/main/java/com/example/link_online_tutoring_app_/FacultiesAcitvities/HealthScienceActivity.java
@@ -0,0 +1,47 @@
+package com.example.link_online_tutoring_app_.FacultiesAcitvities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.link_online_tutoring_app_.ChatActivity;
+import com.example.link_online_tutoring_app_.HomeActivity;
+import com.example.link_online_tutoring_app_.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HealthScienceActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_science);
+
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        //perform item selected
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.private_chat:
+                        startActivity(new Intent(HealthScienceActivity.this, ChatActivity.class));
+                        break;
+
+                    case R.id.action_faculties:
+                        startActivity(new Intent(HealthScienceActivity.this,HomeActivity.class));
+                        finish();
+                        break;
+                }
+
+
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/example/link_online_tutoring_app_/HomeActivity.java
+++ app/src/main/java/com/example/link_online_tutoring_app_/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.link_online_tutoring_app_;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -8,15 +10,20 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.link_online_tutoring_app_.FacultiesAcitvities.CommerceActivity;
+import com.example.link_online_tutoring_app_.FacultiesAcitvities.HealthScienceActivity;
+import com.example.link_online_tutoring_app_.FacultiesAcitvities.HumanitiesActivity;
+import com.example.link_online_tutoring_app_.FacultiesAcitvities.ScienceActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -24,7 +31,10 @@ public class HomeActivity extends AppCompatActivity {
         TextView v = findViewById(R.id.WelcomeHomeTv);
         final ListView list = findViewById(R.id.TheList);
 
-        final BottomNavigationView bnv = findViewById(R.id.bottom_navigation);
+
+
+
+
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("Science");
         arrayList.add("Commerce");
@@ -37,8 +47,50 @@ public class HomeActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String clickedItem=(String) list.getItemAtPosition(position);
                 Toast.makeText(HomeActivity.this,clickedItem, Toast.LENGTH_LONG).show();
+
+                //perform item selected in the list view
+                if(position ==0){
+                    startActivity(new Intent(HomeActivity.this, ScienceActivity.class));
+                    finish();
+                }
+                if(position ==1){
+                    startActivity(new Intent(HomeActivity.this, CommerceActivity.class));
+                    finish();
+
+                }
+                if(position ==2){
+                    startActivity(new Intent(HomeActivity.this, HumanitiesActivity.class));
+                    finish();
+
+                }
+                if(position ==3){
+                    startActivity(new Intent(HomeActivity.this, HealthScienceActivity.class));
+                    finish();
+
+                }
             }
         });
 
+        //initialise And assign variable
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //perform item selected
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.private_chat:
+                        startActivity(new Intent(HomeActivity.this,ChatActivity.class));
+                        break;
+
+                    case R.id.action_faculties:
+                        break;
+                }
+
+
+            }
+        });
     }
+
 }
