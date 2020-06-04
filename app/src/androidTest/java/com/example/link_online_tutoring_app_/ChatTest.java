216f46674355d5e5b2ee108964c2d0fb42adaf59
BL4CKDUDE <61325361+BL4CKDUDE@users.noreply.github.com>
2020-06-04 21:15:56 +0200

    New Chat Test

--- app/src/androidTest/java/com/example/link_online_tutoring_app_/ChatTest.java
+++ app/src/androidTest/java/com/example/link_online_tutoring_app_/ChatTest.java
@@ -1,15 +1,8 @@
 package com.example.link_online_tutoring_app_;
 
-import android.app.Activity;
-import android.app.Instrumentation;
-import android.widget.Button;
-import android.widget.EditText;
-
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -20,12 +13,9 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-import static org.junit.Assert.*;
+
 @RunWith(AndroidJUnit4.class)
-public class ChatActivityTest {
+public class ChatTest {
     @Test
     public void AClickMessagePass() {
         ActivityScenario<ChatActivity> sne = ActivityScenario.launch(ChatActivity.class);
