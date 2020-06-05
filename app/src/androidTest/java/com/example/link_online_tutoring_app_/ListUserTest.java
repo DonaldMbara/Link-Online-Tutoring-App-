214af6bc23c0c544f9ff87b1cb501fc519724486
mabhutidlodlo <mabhutidlodlo1@gmail.com>
2020-06-05 04:15:43 +0200

    modifiedtests

--- app/src/androidTest/java/com/example/link_online_tutoring_app_/ListUserTest.java
+++ app/src/androidTest/java/com/example/link_online_tutoring_app_/ListUserTest.java
@@ -1,5 +1,6 @@
 package com.example.link_online_tutoring_app_;
 
+import android.content.Intent;
 import android.widget.LinearLayout;
 
 import androidx.test.core.app.ActivityScenario;
@@ -8,10 +9,13 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
 
 @RunWith(AndroidJUnit4.class)
 public class ListUserTest {
@@ -21,5 +25,11 @@ public class ListUserTest {
         onView(withId(R.id.ls)).check(matches(isDisplayed()));
         onView(withId(R.id.WelcomeHomeTv)).check(matches(isDisplayed()));
         onView(withId(R.id.listUsers)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void clickUser(){
+        ActivityScenario<ListAllUsers> active = ActivityScenario.launch(ListAllUsers.class);
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
     }
 }
